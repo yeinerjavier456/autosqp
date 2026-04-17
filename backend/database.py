c86ef88362f76b3218c782208007687c56b31a0c
Yeiner Javier Bejarano Mora <136617000+yeinerjavierbejaranomora@users.noreply.github.com>
2026-04-17 15:06:19 -0500

    feat: initialize SQLAlchemy database engine and session management configuration

--- backend/database.py
+++ backend/database.py
@@ -9,14 +9,20 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 check_same_thread = False
 if "sqlite" in DATABASE_URL:
-    check_same_thread = False
     connect_args = {"check_same_thread": False}
+    engine = create_engine(
+        DATABASE_URL, connect_args=connect_args
+    )
 else:
     connect_args = {}
-
-engine = create_engine(
-    DATABASE_URL, connect_args=connect_args
-)
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args=connect_args,
+        pool_size=20,
+        max_overflow=40,
+        pool_recycle=1800,
+        pool_pre_ping=True,
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
